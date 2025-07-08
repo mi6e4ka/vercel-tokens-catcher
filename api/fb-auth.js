41536ded0b2006ec23f79dee47f7cbc012d7df67
mi6e4ka <code@mi6e4ka.dev>
2025-07-08 19:13:00 +0300

    add oauth gen

---
+++ api/fb-auth.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  const APP_ID = process.env.APP_ID || "771512691871130"; // Из переменных окружения
+  const REDIRECT_URI = "https://vercel-tokens-catcher.vercel.app/api/fb-webhook";
+  const SCOPES = [
+    "pages_show_list",
+    "business_management",
+    "instagram_basic",
+    "instagram_manage_insights",
+    "pages_read_engagement"
+  ].join(",");
+
+  const params = new URLSearchParams({
+    client_id: APP_ID,
+    redirect_uri: REDIRECT_URI,
+    scope: SCOPES,
+    response_type: "code",
+    display: "popup"
+  });
+
+  const authUrl = `https://www.facebook.com/v23.0/dialog/oauth?${params.toString()}`;
+
+  res.redirect(302, authUrl);
+}
